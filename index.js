ac5dcf2a227b3db2a2b204e74af7251df87779ac
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-05-21 16:10:08 +0500

    lecture intro to advance js done

---
+++ index.js
@@ -0,0 +1,9 @@
+// Lecture #1
+// 1st version of js was ECMAscript 1997
+// european computer manufacturing association:
+// now ECMAscript 5.1 is used:
+// in 2015 ES6 was the major update which used the oops concept in js:
+// till now ES9 is the latest updated version of js:
+// we will cover regular expressions:
+// new advance events and web APIs
+// DOM new methods and properties:
