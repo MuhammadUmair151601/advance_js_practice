76dc12a04ed2d4ad8d1ff891093890e285ea37ac
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-05-22 15:17:57 +0500

    Lecture 2 var let and const advance js done

--- index.js
+++ index.js
@@ -7,3 +7,14 @@
 // we will cover regular expressions:
 // new advance events and web APIs
 // DOM new methods and properties:
+
+// Lecture #2
+// Let and const variables:
+// Let and const were introduced in ES6 version while the var was the old one:
+// in case of var we can re declare it in the code:
+// in case of let we cannot re declare the value with the same name but we can assign diff value:
+// in case of const we cannot change anything:
+// in terms of scope:
+// var has a global scope we can access it anywhere in the code:
+// let can be used in specific part and cannot be accessed outside then:-->  it is block scope:
+// const has the same condition we cannot access it outside the block of the code:
